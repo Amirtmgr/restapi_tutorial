fcbad55d5e02d5fbc9b441d2164c858963713c44
Amir Mgr <athapa@eeposit.com>
2020-07-09 23:54:53 +0545

    Class based api view used.

--- FirstProject/api_basic/urls.py
+++ FirstProject/api_basic/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import article_list
-from .views import article_detail
+# from .views import article_list
+# from .views import article_detail
+from .views import ArticleAPIViews, ArticleDetail
 
 urlpatterns = [
-    path('article/', article_list),
-    path('article_detail/<int:pk>', article_detail)
+
+    path('article/', ArticleAPIViews.as_view()),
+    path('article_detail/<int:id>', ArticleDetail.as_view())
 ]
+
+
+    # path('article/', article_list),
+    # path('article_detail/<int:pk>', article_detail)
